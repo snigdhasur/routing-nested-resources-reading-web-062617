df5f73a86c49dde35450a8a6dad014b5db5c8f50
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-05 18:59:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do 
+  	resources :posts, only: [:show, :index]
+  end 
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+
 end
